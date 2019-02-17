13174c1e5cbcb56103e38da2dc5ed2c69b8cf02f
Aleksandra.Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-02-17 11:02:16 +0000

    new file JavaScript

---
+++ basics.js
@@ -0,0 +1,25 @@
+let dog = 4;
+let cat = 5;
+
+// sample of if statement
+if (dog>6) {
+  console.log("dog is bigger than 6");
+}
+
+// sample of switch statement
+switch (dog) {
+  case 12:
+
+    break;
+  case 6:
+
+    break;
+  default:
+
+}
+
+// sample of cammel notation
+let catAndDog = dog > 6 && cat < 6;
+if (catAndDog == true) {
+  console.log("OK");
+}
