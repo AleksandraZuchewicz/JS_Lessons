17281b0f9edcb685794ee38c1056d083afb2ec51
Aleksandra.Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-02-28 17:08:42 +0000

    Finnished tasks

--- JS_Task_2.js
+++ JS_Task_2.js
@@ -22,5 +22,21 @@ function checkPallindrome(string){
 }
 console.log(checkPallindrome(testString));
 console.log(checkPallindrome("anagra"));
-//Task.3 Prepare a function which takes and e-mail adress as argument and returns domein part of adress.
+//Task.3 Prepare a function which takes and e-mail adress as argument and returns domain part of adress.
+
+function returnDomain (email) {
+  let position = email.search("@");
+  let domain = email.slice(position+1, email.length);
+  return domain;
+}
+console.log(returnDomain('aleksandra.zuchewicz@gmail.com'));
 //Task.4 Given a string of form number1-number2-number3 etc. Prepare afunction which counts digints in each number and sort list of numbers digits descendlty.
+// 123-4444-9 => 3,4,1 => 4,3,1
+let newArray = "123-4444-9".split("-");
+function countDigits(element){
+  let stringLenght = element.length;
+  return stringLenght;
+}
+let digitsArray = newArray.map(countDigits);
+digitsArray.sort(function(a, b){return b-a});
+console.log(digitsArray);
